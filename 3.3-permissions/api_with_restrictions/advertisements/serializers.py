537ffe138b41219ee796c1164b67facabb6c8290
SauninaIA <irina.a.saunina@gmail.com>
2023-04-11 23:20:45 +0300

    validate_changed

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -28,20 +28,13 @@ class AdvertisementSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         """Метод для создания"""
-
-        # Простановка значения поля создатель по-умолчанию.
-        # Текущий пользователь является создателем объявления
-        # изменить или переопределить его через API нельзя.
-        # обратите внимание на `context` – он выставляется автоматически
-        # через методы ViewSet.
-        # само поле при этом объявляется как `read_only=True`
         validated_data["creator"] = self.context["request"].user
         return super().create(validated_data)
 
     def validate(self, data):
         """Метод для валидации. Вызывается при создании и обновлении."""
         user_id = self.context['request'].user.id
-        adv_list = len(Advertisement.objects.filter(creator=user_id, status='OPEN'))
-        if self.context['request'].method == 'POST' and adv_list > 9:
+        adv_list = Advertisement.objects.filter(creator=user_id, status='OPEN').count()
+        if (self.context['request'].method == 'POST' or data.get('status') == 'OPEN') and adv_list > 9:
             raise ValidationError('Нельзя иметь более 10 открытых объявлений')
         return data
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,13 @@
+import pytest
+from rest_framework.test import APIClient
+
+
+@pytest.mark.django_db
 def test_example():
+    # Arrange
+    client = APIClient()
+
+    # Act
+    response = client.get()
+
     assert False, "Just test example"
